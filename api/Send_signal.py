4b8e0b6990342a6f5013853cc3cfeefe41e08487
Phat Pham <phatpham@berkeley.edu>
2018-01-11 17:09:06 -0800

    Fixed bugs

--- api/Send_signal.py
+++ api/Send_signal.py
@@ -1,23 +1,26 @@
-from googleSheetAPI import retrieveSpreadsheetData
-from Thread import Timer
+from googleSheetAPI import retrieveSpreadsheetData, CLIENT_SECRET_FILE
+from threading import Timer
 
-prev_lenth = -1 #keeps track of the lenth
+prev_length = -1 #keeps track of the length
 
 def check_and_send():
-    nonlocal prev_lenth
+    print("Checking..")
+    global prev_length
     values = retrieveSpreadsheetData("1Lz50t87PPdlXymskt1uNEHd8pP7y9h9Te7i9XJCGfSg", 'A2:M', CLIENT_SECRET_FILE)
-    if len(value) != prev_lenth:
-        prev_lenth = len(value)
-        data = filter(value, lambda x: x[1].lower != yes) #since I don't know how to delete rows, which wh=ould be even better
+    if len(values) != prev_length:
+        prev_length = len(values)
+        data = filter(lambda x: x[1].lower != "yes", values) # Since I don't know how to delete rows, which would be even better.
         for row in data:
             message = "Look, you have a {} priority message from {}: {}".format(row[3], row[5], row[2])
             to = row[4]
             send_message(message, to)
-        return Timer(1800.0, check_and_send).start()
+        return Timer(5.0, check_and_send).start()
 
 
 
 def send_message(message, to):
-    pass #send a get request to our website
+    print(message)
+    print(to)
+    #return #send a get request to our website
 
 check_and_send() #runs for the first time immediately
