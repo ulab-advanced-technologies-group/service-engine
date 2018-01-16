3a37602128f2c60aae962646ec2561c845f0334d
Vaidya <vaidya@calvisitor-10-105-136-207.calvisitor.1918.berkeley.edu>
2018-01-15 20:23:34 -0800

    Completely new skeleton

--- api/Send_signal.py
+++ api/Send_signal.py
@@ -3,24 +3,40 @@ from threading import Timer
 from gmailAPI import send_email
 from Emails import emails
 
-prev_length = -1 #keeps track of the length
+remindDict = {"type of task": "How often"}
+prev_length = len(retrieveSpreadsheetData("1Lz50t87PPdlXymskt1uNEHd8pP7y9h9Te7i9XJCGfSg", 'A2:M', CLIENT_SECRET_FILE)) #keeps track of the length
 
 def check_and_send():
     print("Checking..")
     global prev_length
     values = retrieveSpreadsheetData("1Lz50t87PPdlXymskt1uNEHd8pP7y9h9Te7i9XJCGfSg", 'A2:M', CLIENT_SECRET_FILE)
     if len(values) != prev_length:
+        new_entries = values[:prev_length]
         prev_length = len(values)
-        data = filter(lambda x: x[1].lower != "yes", values) # Since I don't know how to delete rows, which would be even better.
-        for row in data:
-            message = "Look, you have a {} priority message from {}: {}".format(row[3], row[5], row[2])
-            send_message(row[4], "You have been assigned a task", message)
-            send_message(row[5], "You have just assigned {} a task".format(row[4]), "Task has been assgined")
-        return Timer(1800.0, check_and_send).start() #Ask Amit, again
+        for row in new_entries:
+            send_message(row)
+        Timer(10.0, check_and_send).start() #Ask Amit, again
 
-def send_message(to_whom, subject, message):
-    email_to_send = emails[to_whom]
-    send_email("phatpham@berkeley.edu", emails[to_whom], subject, message ) # This could be counted as Spam.
+def send_message(row):
+     message = message_handler(row)
+     for person in message[0]:
+         #Email for the assigned
+         send_email("phatpham@berkeley.edu", message[0][person], message[1], message[2])
+     #Email for requester
+     send_email("phatpham@berkeley.edu", row[11], message[3], message[4])
 
+
+#Takes in request row, and returns [recipients, subject2, message1, subject2, message2]
+def message_handler(row):
+    return
+
+def remind(row):
+    print("Reminding...")
+    when_remind = remindDict[row[2]]
+    if (not when_remind) or (row[1] = "Yes"):
+        return
+    send_message(row)
+    Timer(when_remind, remind, row).start()
 
 check_and_send() #runs for the first time immediately
+Timer(21600, remind).start()
